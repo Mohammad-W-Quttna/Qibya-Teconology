c448a75f475bdfc69a00a8a0de2130b4f4ae8601
MohammadQuttna <mohammadquttna2020@gmail.com>
2022-08-24 00:10:08 +0300

    Step_3

--- Quttna/frontend/src/App.js
+++ Quttna/frontend/src/App.js
@@ -1,13 +1,42 @@
-
+import data from './data';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
       <a href="/"> Qibya Teconlogy</a>
+
       </header>
+      
       <main>
-         list product
-         
+
+        
+          <h1>Featured Products</h1>
+
+<div class="products">
+  
+
+
+{data.products.map(product => (
+<div className="product" key={product.slug}>
+  <a href={`/product/${product.slug}`}>
+
+<img src={product.image} alt={product.name}/> 
+</a>
+     <div className="info">
+     <a href={`/product/${product.slug}`}>
+<p>{product.name}</p>
+</a>
+
+<p>
+  <strong>${product.price}</strong>
+  </p>
+ <button> Add to cart</button>
+    </div>
+
+</div>
+
+))}
+</div>
       </main>
     </div>
   );
