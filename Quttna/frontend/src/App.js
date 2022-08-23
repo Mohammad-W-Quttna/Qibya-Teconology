53b333a35bfd0229ece5064336fd78b774d7cb81
MohammadQuttna <mohammadquttna2020@gmail.com>
2022-08-23 22:44:22 +0300

    VEDIO3

---
+++ Quttna/frontend/src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <a href="/"> Qibya Teconlogy</a>
+      </header>
+      <main>
+         list product
+         
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import moduleName from 'module'
+
+function App(){
+
+return <div className="test">Hello world</div>;
+
+}
+
+export default App;
